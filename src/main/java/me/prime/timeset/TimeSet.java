6d96456140eab561748bab5e611ac54426f3f059
Prime <shreyaanmisra0@gmail.com>
2024-03-15 22:09:51 +0530

    added a command

--- src/main/java/me/prime/timeset/TimeSet.java
+++ src/main/java/me/prime/timeset/TimeSet.java
@@ -3,21 +3,39 @@ package me.prime.timeset;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
 
 public class TimeSet extends JavaPlugin {
 
+    private boolean enabled = false;
+
     @Override
     public void onEnable() {
-        // Set the time to day (1000) for all worlds
-        for (World world : Bukkit.getWorlds()) {
-            world.setTime(1000);
-        }
-
-        // Prevent the time from changing
+        Objects.requireNonNull(getCommand("ts")).setExecutor(this);
         getServer().getScheduler().runTaskTimer(this, () -> {
-            for (World world : Bukkit.getWorlds()) {
-                world.setTime(1000);
+            if (enabled) {
+                for (World world : Bukkit.getWorlds()) {
+                    world.setTime(1000);
+                }
             }
         }, 20, 20);
+    }
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, Command command, @NotNull String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("ts")) {
+            enabled = !enabled;
+            if (enabled) {
+                sender.sendMessage("§aTimeSet Enabled");
+            } else {
+                sender.sendMessage("§cTimeSet Disabled");
+            }
+            return true;
+        }
+        return false;
     }
 }
